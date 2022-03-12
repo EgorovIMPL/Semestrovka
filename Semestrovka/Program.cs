c6b80637cdfb8fac86167b7b0fbfb9b017f401e5
EgorovIMPL <egorov.r.684017@gmail.com>
2022-03-12 18:50:57 +0300

    .

---
+++ Semestrovka/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Semestrovka
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("I want chicken burger");
+            Mama.Sum();
+        }
+    }
+
+    public static class Mama
+    {
+        public static void Sum()
+        {
+            Console.WriteLine("NO.");
+        }
+    }
+}
