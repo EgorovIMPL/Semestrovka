585682b0fd26f22b449dcd50614bbca2ad4c4133
EgorovIMPL <egorov.r.684017@gmail.com>
2022-03-23 21:30:07 +0300

    .

---
+++ Semestrovka/Generation.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace Semestrovka;
+
+public class Generation
+{
+    public static void Start()
+    {
+        string[] Data = new string[new Random().Next(50, 100)];
+        for (int i = 0; i < Data.Length; i++)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(new Random().Next(1, 50) + " " + new Random().Next(1, 50));
+            for (int g = 0; g < new Random().Next(100, 1000); g++)
+            {
+                sb.Append("," + new Random().Next(1, 50) + " " + new Random().Next(1, 50));
+            }
+            Data[i] = sb.ToString();
+        }
+        File.WriteAllLines(@"C:\SomeDir2\Semestrovka.txt", Data);
+    }
+
+    public static string[][] FileReading(string path)
+    {
+        string[] file = File.ReadAllLines(path);
+        string[][] result = new string[file.Length][];
+        for (int i = 0; i < file.Length; i++)
+            result[i] = file[i].Split(",");
+        return result;
+    }
+
+    public static Point[] ArraySetPoints(int stringNumber)
+    {
+        string[][] str = FileReading(@"C:\SomeDir2\Semestrovka.txt");
+        if (stringNumber >= str.Length)
+            throw new Exception("Выход за массив");
+        Point[] result = new Point[str[stringNumber-1].Length];
+        for (int i = 0; i < str[stringNumber-1].Length; i++)
+        {
+            int[] s = Array.ConvertAll(str[stringNumber-1][i].Split(" "),Int32.Parse);
+            result[i] = new Point(s[0],s[1]);
+        }
+        return result;
+    }
+}
--- Semestrovka/Program.cs
+++ Semestrovka/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Numerics;
+using System.Runtime.Intrinsics;
 
 namespace Semestrovka
 {
@@ -6,16 +9,68 @@ namespace Semestrovka
     {
         static void Main()
         {
-            Console.WriteLine("I want chicken burger");
-            Mama.Sum();
+            
+            int n = 1;
+            Point[] array = UniquePoints(Generation.ArraySetPoints(n));
+            PointPrint(array);
+            PointPrint(Greham.ConvexHull(array));
+            Point[] array1 = UniquePoints(Generation.ArraySetPoints(n));
+            PointPrint(Chan.ConvexHull(array1));
+        }
+
+        public static void Print(string[][] str)
+        {
+            foreach (var el in str)
+            {
+                Console.WriteLine(String.Join(" ",el));
+            }
+        }
+
+        public static void PointPrint(Point[] array)
+        {
+            Console.WriteLine();
+            foreach (var el in array)
+            {
+                Console.Write(el.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        public static Point[] UniquePoints(Point[] array)
+        {
+            Couple[] library = new Couple[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+                foreach (var el in library)
+                    if (array[i].X == el.Inf.X && array[i].Y == el.Inf.Y)
+                    {
+                        el.Count += 1;
+                        count += 1;
+                        break;
+                    }
+                if (count == 0)
+                    library = library.Append(new Couple(array[i])).ToArray();
+            }
+
+            Point[] result = new Point[0];
+            foreach (var couple in library)
+            {
+                result = result.Append(couple.Inf).ToArray();
+            }
+
+            return result;
+        }
+    }
+    public class Couple
+    {
+        public Point Inf;
+        public int Count;
+        public Couple(Point inf)
+        {
+            Inf = inf;
+            Count = 1;
         }
     }
 
-    public static class Mama
-    {
-        public static void Sum()
-        {
-            Console.WriteLine("NO.");
-        }
-    }
 }
